2e6501285a92866bc9b21b8ab34420a69844c4e0
griev04 <18719688+griev04@users.noreply.github.com>
2020-12-12 13:03:13 +0100

    add day 12 solutions

--- src/day12/day12.kt
+++ src/day12/day12.kt
@@ -7,28 +7,36 @@ fun main() {
     val instructions = TextFileParser.parseLines("src/day12/input.txt") { Pair(it[0], it.takeLast(it.length - 1).toInt()) }
     println("Part 1")
     println(findDistance(instructions))
+
+    println("Part 2")
+    println(findDistance(instructions, true))
 }
 
-fun findDistance(instructions: List<Pair<Char, Int>>): Int {
-    var currentDirection = 'E'
-    var dLong = 0
-    var dLat = 0
+fun findDistance(instructions: List<Pair<Char, Int>>, isPart2: Boolean = false): Int {
+    var currentVector = if (isPart2) {
+        Pair(1, -10)
+    } else {
+        Direction.Companion.DirectionVector.E.vector
+    }
+    var dLat = 0; var dLong = 0
 
     instructions.forEach { instruction ->
-        val (dir, value) = instruction
-        when (dir) {
+        val (command, value) = instruction
+        when (command) {
             'F' -> {
-                val (lat, long) = Direction.getDisplacement(currentDirection, value)
-                dLat += lat
-                dLong += long
+                val (lat, long) = Direction.getDisplacement(currentVector, value)
+                dLat += lat; dLong += long
             }
-            'L', 'R' -> currentDirection = Direction.turn(currentDirection, dir, value)
+            'L', 'R' -> currentVector = Direction.turn(currentVector, command, value)
             else -> {
-                val (lat, long) = Direction.getDisplacement(dir, value)
-                dLat += lat
-                dLong += long
+                val direction = Direction.Companion.DirectionVector.valueOf(command.toString()).vector
+                val (lat, long) = Direction.getDisplacement(direction, value)
+                if (isPart2) {
+                    currentVector = Pair(currentVector.first + lat, currentVector.second + long)
+                } else {
+                    dLat += lat; dLong += long
+                }
             }
-
         }
     }
 
@@ -37,30 +45,31 @@ fun findDistance(instructions: List<Pair<Char, Int>>): Int {
 
 class Direction {
     companion object {
-        private const val directionsSequence = "NESW"
-        private const val numberOfDirections = directionsSequence.length
-        private val directionCoordinates = mapOf(
-                'N' to Pair(0, 1),
-                'E' to Pair(-1, 0),
-                'S' to Pair(0, -1),
-                'W' to Pair(1, 0)
-        )
-
-        fun getDisplacement(direction: Char, value: Int): Pair<Int, Int> {
-            return Pair(
-                    value * (directionCoordinates[direction]?.first ?: 0),
-                    value * (directionCoordinates[direction]?.second ?: 0)
-            )
+        enum class DirectionVector(val vector: Pair<Int, Int>) {
+            N(Pair(1, 0)),
+            E(Pair(0, -1)),
+            S(Pair(-1, 0)),
+            W(Pair(0, 1))
         }
 
-        fun turn(facingDirection: Char, turningDirection: Char, degrees: Int): Char {
-            val dDir = (degrees % 360) / 90
-            val newIdx = if (turningDirection == 'L') {
-                directionsSequence.indexOf(facingDirection) - dDir + numberOfDirections
-            } else {
-                directionsSequence.indexOf(facingDirection) + dDir
+        fun getDisplacement(vector: Pair<Int, Int>, value: Int): Pair<Int, Int> {
+            return Pair(value * (vector.first), value * (vector.second))
+        }
+
+        fun turn(facingDirection: Pair<Int, Int>, turningDirection: Char, degrees: Int): Pair<Int, Int> {
+            val turns = (degrees % 360) / 90
+            if (turns == 0) return facingDirection
+
+            var newDirection = facingDirection
+
+            for (t in 1..turns) {
+                newDirection = if (turningDirection == 'R') {
+                    Pair(newDirection.second, -newDirection.first)
+                } else {
+                    Pair(-newDirection.second, newDirection.first)
+                }
             }
-            return directionsSequence[newIdx % numberOfDirections]
+            return newDirection
         }
     }
 }
