54c8ef0a4a86d46a5c275c2da160140fd33e6b11
griev04 <18719688+griev04@users.noreply.github.com>
2020-12-15 22:23:03 +0100

    add day 15 solutions

---
+++ src/day15/day15.kt
@@ -0,0 +1,31 @@
+package day15
+
+import common.TextFileParser
+
+fun main() {
+    val sequence = TextFileParser.parseFile("src/day15/input.txt") { parse(it) }
+
+    val res1 = playMemoryGame(30000000, sequence)
+    println(res1)
+}
+
+fun playMemoryGame(turns: Int, seed: List<Int>): Int {
+    val seen = seed.take(seed.size - 1).mapIndexed { index, el -> el to index + 1 }.toMap().toMutableMap()
+    var previous: Int
+    var lastSpoken = seed.last()
+
+    for (turn in seed.size + 1..turns) {
+        previous = lastSpoken
+        lastSpoken = if (seen[lastSpoken] == null) {
+            0
+        } else {
+            turn - 1 - seen[lastSpoken]!!
+        }
+        seen[previous] = turn - 1
+    }
+    return lastSpoken
+}
+
+fun parse(numbers: String): List<Int> {
+    return numbers.split("\n")[0].split(",").map { it.toInt() }
+}
