cfed705bda6a0c917b18fe07d189eb8359175543
griev04 <18719688+griev04@users.noreply.github.com>
2020-12-21 09:26:19 +0100

    refactor day 21 solutions

--- src/year2020/day21/day21.kt
+++ src/year2020/day21/day21.kt
@@ -15,15 +15,13 @@ fun main() {
 }
 
 class LabelAnalyzer(private val labels: List<Label>) {
-    private lateinit var allergens: Set<String>
-    private lateinit var ingredients: Set<String>
-    private lateinit var safeIngredients: Set<String>
+    private val ingredients = labels.map { it.ingredients }.flatten().toSet()
+    private val allergens = labels.map { it.allergens }.flatten().toSet()
+    private val safeIngredients = mutableSetOf<String>()
     private val allergensIngredients = mutableMapOf<String, MutableSet<String>>()
 
     init {
-        getAllergensList()
-        getIngredientsList()
-        findSafeIngredients()
+        matchAllergensToIngredients()
     }
 
     fun getDangerousList(): String {
@@ -31,39 +29,31 @@ class LabelAnalyzer(private val labels: List<Label>) {
     }
 
     fun countSafeIngredientsOccurrence(): Int {
-        return safeIngredients.fold(0) { acc, ingredient ->
-            acc + labels.map { it.ingredients }.flatten().count { it == ingredient }
-        }
+        return safeIngredients.map { ingredient ->
+            labels.map { it.ingredients }.flatten().count{ it == ingredient }
+        }.sum()
     }
 
-    private fun findSafeIngredients() {
+    private fun matchAllergensToIngredients() {
         // define possible allergenic ingredients
         allergens.forEach { allergen ->
-            val correspondingLabels = labels.filter { it.allergens.contains(allergen) }
-            val correspondingLabelsIngredients = correspondingLabels.map { label -> label.ingredients.toSet() }.reduce { acc, set -> acc.intersect(set) }
-            allergensIngredients[allergen] = correspondingLabelsIngredients.toMutableSet()
+            val correspondingLabelsIngredients = labels.filter { it.allergens.contains(allergen) }.map { label -> label.ingredients.toSet() }
+            val commonIngredients = correspondingLabelsIngredients.reduce { acc, set -> acc.intersect(set) }
+            allergensIngredients[allergen] = commonIngredients.toMutableSet()
         }
 
         // narrow down to specific allergenic ingredient
         while (allergensIngredients.count { it.value.size > 1 } > 0) {
             val allergenMatches = allergensIngredients.entries.filter { it.value.size == 1 }.map { it.value.first() }
             allergenMatches.forEach { match ->
-                allergensIngredients.filter { it.value.size > 1 }.forEach { (_, v) ->
-                    v.remove(match)
+                allergensIngredients.filter { it.value.size > 1 }.forEach { (_, ingredients) ->
+                    ingredients.remove(match)
                 }
             }
         }
 
         val unsafeIngredients = allergensIngredients.values.flatten().toSet()
-        safeIngredients = ingredients.toMutableSet().minus(unsafeIngredients)
-    }
-
-    private fun getAllergensList() {
-        allergens = labels.map { it.allergens }.flatten().toSet()
-    }
-
-    private fun getIngredientsList() {
-        ingredients = labels.map { it.ingredients }.flatten().toSet()
+        safeIngredients.addAll(ingredients.toMutableSet().minus(unsafeIngredients))
     }
 }
 
