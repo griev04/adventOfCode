88676665b5b0c4e18070cf584384bb1a4a782b43
griev04 <18719688+griev04@users.noreply.github.com>
2021-12-02 11:47:26 +0100

    add day 2 part 2 solution

--- src/year2021/day02/day02.kt
+++ src/year2021/day02/day02.kt
@@ -3,68 +3,102 @@ package year2021.day02
 import common.TextFileParser
 
 fun main() {
-    val input = TextFileParser.parseLines("src/year2021/day02/input.txt") { Instruction.makeFrom(it) }
+    val instructions = TextFileParser.parseLines("src/year2021/day02/input.txt") { Instruction.makeFrom(it) }
 
     println("Day 02 Part 1")
-    val result1 = NavigationSystem().navigateWith(input).getResult()
+    val commandInterpretation1 = CommandInterpretation.PART_ONE
+    val result1 = NavigationSystem().navigateWith(instructions, commandInterpretation1).getResult()
     println(result1)
+
+    println("Day 02 Part 2")
+    val commandInterpretation2 = CommandInterpretation.PART_TWO
+    val result2 = NavigationSystem().navigateWith(instructions, commandInterpretation2).getResult()
+    println(result2)
 }
 
 class NavigationSystem() {
-    private val course: MutableList<Instruction> = mutableListOf()
     private val currentPosition: Position = Position(0, 0)
 
-    fun navigateWith(courseInstructions: List<Instruction>): NavigationSystem {
-        course.clear()
-        course.addAll(courseInstructions)
-        navigate()
+    fun navigateWith(courseInstructions: List<Instruction>, commandInterpretation: CommandInterpretation): NavigationSystem {
+        courseInstructions.forEach { commandInterpretation(it, currentPosition)}
         return this
-    }
-
-    private fun navigate() {
-        course.forEach { instruction -> instruction.executeAt(currentPosition) }
     }
 
     fun getResult(): Long = currentPosition.getPositionValue()
 }
 
-class Position(private var horizontalPosition: Long, private var depth: Long) {
-    fun moveForward(distance: Int) {
-        horizontalPosition += distance
-    }
+class CommandInterpretation(private val rule: (instruction: Instruction, position: Position) -> Position) {
+    operator fun invoke(instruction: Instruction, position: Position): Position = rule(instruction, position)
 
-    fun increaseDepth(distance: Int) {
-        depth += distance
-    }
+    companion object {
+        val PART_ONE = CommandInterpretation { instruction, position ->
+            when (instruction.direction) {
+                Instruction.Direction.FORWARD -> position.moveForward(instruction.distance)
+                Instruction.Direction.UP -> position.decreaseDepth(instruction.distance)
+                Instruction.Direction.DOWN -> position.increaseDepth(instruction.distance)
+            }
+        }
 
-    fun decreaseDepth(distance: Int) {
-        depth -= distance
-    }
-
-    fun getPositionValue(): Long {
-        return horizontalPosition * depth
+        val PART_TWO = CommandInterpretation { instruction, position ->
+            when (instruction.direction) {
+                Instruction.Direction.FORWARD -> {
+                    position.moveForward(instruction.distance)
+                    position.increaseDepthWithAim(instruction.distance)
+                }
+                Instruction.Direction.UP -> position.decreaseAim(instruction.distance)
+                Instruction.Direction.DOWN -> position.increaseAim(instruction.distance)
+            }
+        }
     }
 }
 
-class Instruction private constructor(private val direction: Direction, private val distance: Int) {
-    fun executeAt(position: Position) {
-        return when (direction) {
-            Direction.FORWARD -> position.moveForward(distance)
-            Direction.UP -> position.decreaseDepth(distance)
-            Direction.DOWN -> position.increaseDepth(distance)
-        }
-    }
-
+class Instruction private constructor(val direction: Direction, val distance: Int) {
     companion object {
         fun makeFrom(command: String): Instruction {
             val (direction, distance) = command.split(' ')
             return Instruction(Direction.valueOf(direction.toUpperCase()), distance.toInt())
         }
     }
+
+    enum class Direction {
+        FORWARD,
+        UP,
+        DOWN
+    }
 }
 
-enum class Direction {
-    FORWARD,
-    UP,
-    DOWN
+class Position(private var horizontalPosition: Long, private var depth: Long, private var aim: Int = 0) {
+    fun moveForward(distance: Int): Position {
+        horizontalPosition += distance
+        return this
+    }
+
+    fun increaseDepth(distance: Int): Position {
+        depth += distance
+        return this
+    }
+
+    fun decreaseDepth(distance: Int): Position {
+        depth -= distance
+        return this
+    }
+
+    fun increaseAim(distance: Int): Position {
+        aim += distance
+        return this
+    }
+
+    fun decreaseAim(distance: Int): Position {
+        aim -= distance
+        return this
+    }
+
+    fun increaseDepthWithAim(distance: Int): Position {
+        depth += distance * aim
+        return this
+    }
+
+    fun getPositionValue(): Long {
+        return horizontalPosition * depth
+    }
 }
