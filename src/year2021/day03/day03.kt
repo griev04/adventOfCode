48837585959c35a3a7fa5ca2303d10279264df17
griev04 <18719688+griev04@users.noreply.github.com>
2021-12-03 10:16:04 +0100

    refactor day 3 solution

--- src/year2021/day03/day03.kt
+++ src/year2021/day03/day03.kt
@@ -5,8 +5,7 @@ import kotlin.math.pow
 
 fun main() {
     val input = TextFileParser.parseLines("src/year2021/day03/input.txt") { it }
-    val report = DiagnosticReport.makeFrom(input)
-
+    val report = DiagnosticReport(input.map { Reading.makeFromBinary(it) })
 
     println("Day 03 Part 1")
     val result1 = report.getPowerConsumption()
@@ -23,15 +22,6 @@ class DiagnosticReport(private val readings: List<Reading>) {
     private val gamma: Reading = computeGamma()
     private val epsilon: Reading = gamma.getComplementaryValue()
 
-    private fun computeGamma(): Reading {
-        val readingsCount = readingsCount
-        val result = LongArray(readingSize)
-        readings.forEach { r ->
-            r.values.forEachIndexed { index, b -> if (b) result[index] = result[index] + 1 }
-        }
-        return Reading.make(result.map { it >= readingsCount / 2.0 })
-    }
-
     fun getPowerConsumption(): Long {
         return gamma.getDecimalRepresentation() * epsilon.getDecimalRepresentation()
     }
@@ -39,48 +29,51 @@ class DiagnosticReport(private val readings: List<Reading>) {
     fun getLifeSupportRating(): Long = getOxygenRate() * getCO2ScrubRate()
 
     private fun getOxygenRate(): Long {
-        return computeRate(false).readings.first().getDecimalRepresentation()
-    }
-
-    private fun getCO2ScrubRate(): Long {
         return computeRate(true).readings.first().getDecimalRepresentation()
     }
 
-    private fun computeRate(isCO2: Boolean, position: Int = 0, report: DiagnosticReport = this): DiagnosticReport {
+    private fun getCO2ScrubRate(): Long {
+        return computeRate(false).readings.first().getDecimalRepresentation()
+    }
+
+    private fun computeRate(isOxygenRate: Boolean, position: Int = 0, report: DiagnosticReport = this): DiagnosticReport {
         if (report.readingsCount == 1 || position >= report.readingSize) {
             return report
         }
-        val targetValue = if (isCO2) report.epsilon.values[position] else report.gamma.values[position]
-        val newReadings = report.readings.filter {
-            it.values[position] == targetValue
-        }
+        val targetValue = if (isOxygenRate) report.gamma.values[position] else report.epsilon.values[position]
+        val matchedReadings = report.readings.filter { it.values[position] == targetValue }
 
-        return computeRate(isCO2, position + 1, DiagnosticReport(newReadings))
+        return computeRate(isOxygenRate, position + 1, DiagnosticReport(matchedReadings))
     }
 
-    companion object {
-        fun makeFrom(input: List<String>): DiagnosticReport {
-            return DiagnosticReport(input.map { Reading.makeFromBinary(it) })
-        }
+    private fun computeGamma(): Reading {
+        val result = transpose(readings).map { reading -> reading.values.sumBy { if (it) 1 else 0 }}
+        return Reading.make(result.map { it >= readingsCount / 2.0 })
+    }
+
+    private fun transpose(readings: List<Reading>): List<Reading> {
+        val start = readings.first().values.map { mutableListOf<Boolean>() }
+        return readings.fold(start) { acc, reading ->
+            reading.values.forEachIndexed { index, b ->
+                acc[index].add(b)
+            }
+            acc
+        }.map { Reading.make(it) }
     }
 }
 
 class Reading private constructor(val values: List<Boolean>) {
-    fun getReadingLength(): Int {
-        return values.size
-    }
+    fun getReadingLength(): Int = values.size
 
     fun getComplementaryValue(): Reading = make(values.map { !it })
-
-    private fun getBinaryRepresentation(): String = values.map { if (it) '1' else '0' }.toString()
 
     fun getDecimalRepresentation(): Long {
         return values.reversed().mapIndexed { index, b -> if (b) 2.0.pow(index.toDouble()) else 0.0 }.sum().toLong()
     }
 
     companion object {
-        fun makeFromBinary(binaryValue: String): Reading {
-            return Reading(binaryValue.map { it == '1' })
+        fun makeFromBinary(binaryRepresentation: String): Reading {
+            return Reading(binaryRepresentation.map { it == '1' })
         }
 
         fun make(booleans: List<Boolean>): Reading {
