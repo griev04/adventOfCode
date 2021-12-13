a030909a542574aadde45facbb9f284066ef73b5
griev04 <18719688+griev04@users.noreply.github.com>
2021-12-13 21:21:15 +0100

    add day 8 part 2 solution

--- src/year2021/day08/day08.kt
+++ src/year2021/day08/day08.kt
@@ -6,41 +6,61 @@ fun main() {
     val entries = TextFileParser.parseLines("src/year2021/day08/input.txt") { Entry.makeFrom(it) }
 
     println("Day 08 Part 1")
-    val result1 = entries.map { it.countUniqueDigits() }.sum()
+    val result1 = entries.sumBy { it.countUniqueDigits() }
     println(result1)
 
     println("Day 08 Part 2")
-    val result2 = ""
+    val result2 = entries.sumBy { it.getDecodedNumber() }
     println(result2)
 }
 
 class Entry(private val patterns: Set<String>, private val encodedDigits: List<String>) {
-    private lateinit var digits: List<Int>
-
-    init {
-        decode()
-        countUniqueDigits()
-    }
 
     fun countUniqueDigits(): Int {
-        val okLengths = lengthToDigitMap.keys
+        val okLengths = lengthToDigitMap.map { it.first }
         return encodedDigits.map {
             if (okLengths.contains(it.length)) 1 else 0
         }.sum()
     }
 
-    private fun decode() {
-        val numbers = mutableMapOf<Int, Set<Char>>()
-        val segments = Array(7) { ' ' }
-        lengthToDigitMap.entries.forEach { entry ->
-            numbers[entry.value] = patterns.first { it.length == entry.key }.toSet()
+    fun getDecodedNumber() = decodePattern()
+
+    private fun decodePattern(): Int {
+        val segments = defineSegments()
+
+        val digitBySegments = mapSegmentsToDigits(segments)
+
+        return encodedDigits.map { string -> digitBySegments[string.toSet()] }
+            .joinToString("").toInt()
+    }
+
+    private fun mapSegmentsToDigits(segments: CharArray) =
+        segmentsByDigit.map { s -> s.map { c -> segments[c.toString().toInt()] }.toSet() }
+            .mapIndexed { index, chars -> chars to index }.toMap()
+
+    private fun defineSegments(): CharArray {
+        // define unique numbers
+        val numbers = lengthToDigitMap.associate { entry ->
+            entry.second to patterns.first { it.length == entry.first }.toSet()
         }
+
+        val segments = CharArray(7) { ' ' }
+        // find segment 0
         segments[0] = (numbers[7]!! - numbers[1]!!).first()
-        val n690 = patterns.filter { it.length == 6 }
-        segments[5] = n690.fold(n690.first().toSet()) { acc, s ->
-            acc.intersect(s.toSet())
-        }.intersect(numbers[1]!!).first()
-        segments[2] = numbers[1]!!.minus(segments[5]).first()
+
+        // remove segment 0 and map others
+        val patternsByLength = patterns.joinToString("").filter { it != segments[0] }.groupBy { it }
+            .map { entry -> entry.value.size to entry.key }.toMap()
+
+        // find missing segments
+        segments[1] = patternsByLength[6]!!
+        segments[2] = patternsByLength[8]!!
+        segments[4] = patternsByLength[4]!!
+        segments[5] = patternsByLength[9]!!
+        segments[3] = (numbers[4]!! - segments[1] - segments[2] - segments[5]).first()
+        segments[6] = numbers[8]!!.minus(segments.toSet()).first()
+
+        return segments
     }
 
     companion object {
@@ -49,6 +69,9 @@ class Entry(private val patterns: Set<String>, private val encodedDigits: List<S
             return Entry(patterns.toSet(), digits)
         }
 
-        val lengthToDigitMap = listOf((2 to 1), (4 to 4), (3 to 7), (7 to 8)).toMap()
+        val lengthToDigitMap = listOf((2 to 1), (4 to 4), (3 to 7), (7 to 8))
+
+        val segmentsByDigit =
+            listOf("012456", "25", "02346", "02356", "1235", "01356", "013456", "025", "0123456", "012356")
     }
 }
